9654bd96bf46c6b2f1f9a4579bf4ddf4f997039b
Robert <robertmanukovich@gmail.com>
2018-03-24 15:42:05 +0400

    new fixes

---
+++ DiplomaWork/app/src/main/java/wda/com/diplomawork/core/usersRecycler/MyAdapter.java
@@ -0,0 +1,64 @@
+package wda.com.diplomawork.core.usersRecycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import wda.com.diplomawork.R;
+import wda.com.diplomawork.core.realM.User;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<User> users;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View root;
+        public TextView nameTextView;
+        public TextView emailTextView;
+        public ViewHolder(View root) {
+            super(root);
+            this.root = root;
+            nameTextView = root.findViewById(R.id.name_text_view);
+            emailTextView = root.findViewById(R.id.email_text_view);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v =  LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_list_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        User user = users.get(position);
+        holder.nameTextView.setText(user.getFirstName() + " "  + user.getLastName());
+        holder.emailTextView.setText(user.getLogin());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+}
--- DiplomaWork/app/src/main/java/wda/com/diplomawork/ui/LoggedInActivity.java
+++ DiplomaWork/app/src/main/java/wda/com/diplomawork/ui/LoggedInActivity.java
@@ -2,14 +2,76 @@ package wda.com.diplomawork.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import wda.com.diplomawork.R;
+import wda.com.diplomawork.base.BaseActivity;
+import wda.com.diplomawork.core.realM.User;
+import wda.com.diplomawork.core.realM.UserRM;
+import wda.com.diplomawork.core.usersRecycler.MyAdapter;
 
-public class LoggedInActivity extends AppCompatActivity {
+public class LoggedInActivity extends BaseActivity {
+
+    private List<User> allUsers;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        recyclerViewConfigs();
+        getAllUsersFromFirebaseDataBase();
+    }
+
+    private void recyclerViewConfigs() {
+        allUsers = new ArrayList<>();
+        adapter = new MyAdapter(allUsers);
+        RecyclerView usersListView = findViewById(R.id.user_list_view);
+        usersListView.setHasFixedSize(true);
+        usersListView.setLayoutManager(new LinearLayoutManager(this));
+        usersListView.setAdapter(adapter);
+    }
+
+    public void getAllUsersFromFirebaseDataBase() {
+        myRef = database.getReference().child("users");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                HashMap<String, User> userMap = (HashMap<String, String>)dataSnapshot.getValue();
+//                userFromFDB.setFirstName(userMap.get("firstName"));
+//                userFromFDB.setLastName(userMap.get("lastName"));
+//                userFromFDB.setLogin(userMap.get("login"));
+                convertHashMapToUsers(dataSnapshot.getValue());
+                adapter.notifyDataSetChanged();
+                findViewById(R.id.progress_layout).setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("");
+            }
+        });
+    }
+
+    private void convertHashMapToUsers(Object value) {
+        Set<String> keys = ((HashMap)value).keySet();
+        for (Iterator<String> iterator = keys.iterator(); iterator.hasNext(); ){
+            String key = iterator.next();
+            HashMap<String, String>  map = (HashMap<String, String>) ((HashMap)value).get(key);
+            allUsers.add(new User(map.get("firstName"), map.get("lastName"), map.get("login")));
+        }
+
     }
 }
